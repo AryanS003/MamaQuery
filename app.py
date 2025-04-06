1ff7d7fcba87280cac6065cd9b28c8173e1b9cc7
Aryan Shishodia <97734166+AryanS003@users.noreply.github.com>
2025-04-06 22:04:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,27 +8,32 @@ import os
 @st.cache_resource
 def load_qa_chain():
     try:
-        model_path = os.path.join(os.path.dirname(__file__), "models", "all-MiniLM-L6-v2")
-        embedder = SentenceTransformer(model_path)
+        # looading model from hf
+        model_name = "justChills/MiniLM-L6-v2-MamaQuery" 
+        st.write(f"Loading model from Hugging Face: {model_name}")
+        
+        embedder = SentenceTransformer(model_name)
+        
+        st.write("Model loaded successfully!")
+    
     except Exception as e:
-        st.error(f"failed loading embedding model {e}")
+        st.error(f"failed to load embeddings: {e}")
         return None
     try:
         index_path = os.path.join(os.path.dirname(__file__), "faiss_index")
         vector_store = FAISS.load_local(index_path, embedder, allow_dangerous_deserialization=True)
     except Exception as e:
-        st.error(f"failed loading FAISS index: {e}")
+        st.error(f"Failed loading FAISS index: {e}")
         return None
-
-        
+    
     API_KEY = st.secrets["GOOGLEAI_API_KEY"]
     
+        
     llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-exp", google_api_key=API_KEY)
     
-    
     retriever = vector_store.as_retriever(search_kwargs={"k": 3})
-    
     qa_chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+    
     return qa_chain
 
 qa_chain = load_qa_chain()
@@ -40,12 +45,12 @@ st.write("Let me help you with your question about Mamaearth and its products!")
 query = st.text_input("Enter your question:", key="query_input")
 if st.button("Submit"):
     if query:
-        with st.spinner("Waiting for response..."):
+        with st.spinner("generating response...."):
             try:
                 result = qa_chain.run(query)
                 answer = result if isinstance(result, str) else result.get("result", "No answer found")
                 st.write("Answer:", answer)
             except Exception as e:
-                st.error(f"Error in processing query: {e}")
+                st.error(f"Error processing query: {e}")
     else:
         st.warning("Please enter a question")
